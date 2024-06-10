03e86b2cbe12818e561ac57e34321cca49766011
Emerson Vieira <emersondont@gmail.com>
2024-06-10 18:02:00 -0300

    refactor: Update Logo component background color class name

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ const font = Playfair_Display({ subsets: ["latin"] });
 export default function Logo() {
   return (
     <div className={`flex cursor-pointer gap-2 items-center text-base font-bold ${font.className}`}>
-      <div className="p-2 rounded bg-black80 text-white">
+      <div className="p-2 rounded bg-black80 text-white100">
         <p>News</p>
       </div>
       <p>Portal</p>
