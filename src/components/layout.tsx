b4e97deed004958c539fdacbbf1068b377ad387c
Emerson Vieira <emersondont@gmail.com>
2024-06-10 18:02:14 -0300

    feat: Add Layout component for consistent page structure

---
+++ src/components/layout.tsx
@@ -0,0 +1,17 @@
+import { ViewTransitions } from 'next-view-transitions'
+
+import Header from "./header";
+
+interface Props {
+  children: React.ReactNode;
+}
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <Header />
+      <main className={`flex min-h-screen flex-col px-32`}>
+        {children}
+      </main>
+    </>
+  )
+}
