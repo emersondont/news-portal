fdc9a866d1f3fac655f91517130e9d82333f3efd
Emerson Vieira <emersondont@gmail.com>
2024-06-10 18:01:54 -0300

    chore: Update Header component with logo link and hover animations

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,17 @@
 import { IoSearch, IoMenu } from "react-icons/io5";
 import Logo from "./logo";
+import Link from "next/link";
 
 
 export default function Header() {
   return (
-    <header className="flex py-8 px-32 w-full justify-between items-center fixed top-0 left-0">
-      <Logo />
+    <header className="flex py-8 px-32 bg-white100 w-full justify-between items-center fixed top-0 left-0 z-10">
+      <Link href='/'>
+        <Logo />
+      </Link>
       <div className="flex gap-8 text-black60">
-        <IoSearch size={28} className="cursor-pointer hover:text-black40 ease-in duration-100"/>
-        <IoMenu size={28} className="cursor-pointer hover:text-black40 ease-in duration-100"/>
+        <IoSearch size={28} className="cursor-pointer hover:scale-110 ease-in duration-100" />
+        <IoMenu size={28} className="cursor-pointer hover:scale-110 ease-in duration-100" />
       </div>
     </header>
   )
