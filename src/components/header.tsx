b70f307ea4fbe10f7d1df8a2660323ad2c0d82a7
Emerson Vieira <emersondont@gmail.com>
2024-06-10 19:50:32 -0300

    feat: Add search functionality to Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,60 @@
-import { IoSearch, IoMenu } from "react-icons/io5";
+import { IoSearch, IoClose } from "react-icons/io5";
 import Logo from "./logo";
 import Link from "next/link";
-
+import { useState } from "react";
+import { roboto } from "@/utils/fonts";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const [openInput, setOpenInput] = useState(false)
+  const [search, setSearch] = useState('')
+  const router = useRouter()
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && search !== '') {
+      router.push({
+        pathname: '/search',
+        query: { q: JSON.stringify(search) }
+      })
+      handleCloseInput()
+    }
+    else if (e.key === 'Escape') {
+      handleCloseInput()
+    }
+  }
+
+  const handleCloseInput = () => {
+    setSearch('')
+    setOpenInput(false)
+  }
+
   return (
     <header className="flex py-8 px-32 bg-white100 w-full justify-between items-center fixed top-0 left-0 z-10">
-      <Link href='/'>
+      <Link href='/' onClick={handleCloseInput}>
         <Logo />
       </Link>
-      <div className="flex gap-8 text-black60">
-        <IoSearch size={28} className="cursor-pointer hover:scale-110 ease-in duration-100" />
-        <IoMenu size={28} className="cursor-pointer hover:scale-110 ease-in duration-100" />
+      <div className={"flex gap-4 border rounded p-1 " + (openInput ? 'border-black80' : 'border-transparent')}>
+        {
+          openInput && (
+            <input
+              type="text"
+              className={`outline-none text-black80 rounded ${roboto.className}`}
+              placeholder="Pesquisar..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onKeyDown={handleKeyPress}
+              autoFocus={true}
+            />
+          )
+        }
+        <button onClick={() => setOpenInput(!openInput)}>
+          {
+            openInput ?
+              <IoClose size={28} className="cursor-pointer hover:scale-110 ease-in duration-100" />
+              :
+              <IoSearch size={28} className="cursor-pointer hover:scale-110 ease-in duration-100" />
+          }
+        </button>
       </div>
     </header>
   )
---
+++ src/pages/search.tsx
@@ -0,0 +1,57 @@
+import NewsCard from "@/components/newsCard";
+import Title from "@/components/title";
+import { Article } from "@/types";
+import { Roboto } from "next/font/google";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export default function Search() {
+  const [news, setNews] = useState<Article[]>([])
+  const router = useRouter()
+  const { query: { q } } = router
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(q)
+      const data = await getData(q as string)
+      console.log(data)
+      setNews(data)
+    }
+    fetchData()
+  }, [q])
+
+  return (
+    <>
+      <Title>{JSON.parse(q as string)}</Title>
+
+      <div className="flex flex-wrap w-full justify-between mt-6">
+        {news.map((article, index) => (
+          <NewsCard key={index} article={article} />
+        ))}
+      </div>
+      {
+        news.length === 0 &&
+        <p className={`text-black80 text-xl mt-6 text-center ${roboto.className}`}>
+          No news found
+        </p>
+      }
+    </>
+  )
+}
+
+async function getData(q: string): Promise<Article[]> {
+  const res = await fetch(`api/searchNews?q=${q}`)
+  const obj = await res.json()
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return obj.data.articles
+}
