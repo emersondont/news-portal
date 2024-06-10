329371e5f2ed9ede9dcecf66fd7502782f502062
Emerson Vieira <emersondont@gmail.com>
2024-06-10 18:01:22 -0300

    chore: Update next.config, package.json and create latestnews

---
+++ src/pages/api/latestNews.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  data: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+
+  try {
+    const response = await axios.get('https://newsapi.org/v2/top-headlines', {
+      params: {
+        apiKey: '216692568f3e47faa19ce006494b59be',
+        sortBy: 'publishedAt',
+        country: 'us',
+        pageSize: 21
+      }
+    });
+
+    res.status(200).json({ data: response.data });
+  } catch (error) {
+    res.status(500).json({ data: 'Error fetching data' });
+  }
+}
